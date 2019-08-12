8eca519ede280d3955fd5e23007e48140f118fe5
Emil Djupvik <emil14833@gmail.com>
2019-08-12 19:26:35 +0200

    add UI/UX

---
+++ lib/routes/auth_route.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AuthRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Signin Demo"),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {},
+          color: Colors.white,
+          child: Container(
+            width: 188.0,
+            height: 32.0,
+            alignment: Alignment.centerLeft,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  "assets/google_logo.png",
+                  width: 22.0,
+                ),
+                SizedBox(width: 8.0),
+                Text(
+                  "Sign in with Google",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
